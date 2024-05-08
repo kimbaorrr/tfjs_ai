525af1b3fe8aef246b917d16255bf1c0dd3de241
Bao <nguyenkimbao.0708@gmail.com>
2024-05-09 01:54:09 +0700

    Initial commit

--- static/predict.js
+++ static/predict.js
@@ -8,8 +8,11 @@ var img = document.getElementById('img-to-show');
 
 
 async function loadModel() {
+    /**
+     * Tải mô hình
+     */
     try {
-        document.getElementById('xem-them').style.display = "none";
+        hideXemThem();
         loadSpin();
         scrollDown();
         model = await tf.loadLayersModel(project.model);
@@ -20,21 +23,39 @@ async function loadModel() {
 };
 
 function loadImage(event) {
+    /**
+     * Upload ảnh từ máy người dùng
+     */
+    let wrong = false;
     file = event.target.files[0];
+    // Kiểm tra tệp có phải là ảnh hay không ?
     if (file.type.split('/')[0] !== 'image') {
         alert('Tệp tải lên phải là một ảnh !');
-        return;
+        wrong = true;
     }
+    // Kiểm tra dung lượng tệp tải lên
     if (file.size > 10000000) {
         alert('Dung lượng của ảnh phải < 10MB !');
-        return;
+        wrong = true;
     }
-    img.src = URL.createObjectURL(file);
-    img.width, img.height = image_size;
+    // Nếu không có lỗi thì hiện ảnh, enable button dự đoán và ngược lại
+    if (!wrong) {
+        enablePredBtn();
+        // Hiển thị ảnh người dùng vừa upload
+        img.src = URL.createObjectURL(file);
+        img.width, img.height = image_size;
+    }
+    else {
+        disablePredBtn();
+    }
 };
 
 function preProcessingImage() {
+    /**
+     * Tiền xử lý ảnh đầu vào trước khi đưa vào mô hình
+     */
     let offset = tf.scalar(127);
+    // Scale ảnh về dạng 0-1,thực hiện resize ảnh  & mở rộng chiều của ảnh
     let tensor = tf.browser.fromPixels(img)
         .resizeNearestNeighbor(image_size)
         .toFloat().sub(offset).div(offset).expandDims(0);
@@ -42,23 +63,53 @@ function preProcessingImage() {
 };
 
 function loadSpin() {
+    /**
+     * Hiện circle loadings
+     */
     document.getElementById("best-result").style.display = "none";
     document.getElementById("loading-spin").style.display = "block";
 }
 
 function hideSpin() {
+    /**
+     * Ẩn circle loadings
+     */
     document.getElementById("best-result").style.display = "block";
     document.getElementById("loading-spin").style.display = "none";
+}
+
+function enablePredBtn() {
+    document.getElementById("pred-btn").disabled = false;
+}
+
+function disablePredBtn() {
+    document.getElementById("pred-btn").disabled = true;
+}
+
+function hideXemThem() {
+    document.getElementById('xem-them').style.display = "none";
+}
+
+function showXemThem() {
+    document.getElementById('xem-them').style.display = "block";
 }
 
 function scrollDown() {
     let x = $(window).scrollTop();
     $('html, body').animate({ scrollTop: x + 600 })
 }
+
 async function predictImage() {
+    /**
+     * Dự đoán ảnh đầu vào & xuất kết quả dự đoán
+     */
+    // Tải mô hình
     await loadModel();
+    // Tiền xử lý ảnh đầu vào
     let tensor = preProcessingImage();
+    // Bắt đầu dự đoán & xuất kết quả
     let predictions = await model.predict(tensor).data();
+    // Mapping dạng key:value, sắp xếp kết quả dự đoán theo thứ tự giảm dần & chỉ lấy tối đa 5 kết quả đầu ra có acc cao nhất
     let results = Array.from(predictions)
         .map(function (p, i) {
             return {
@@ -68,46 +119,68 @@ async function predictImage() {
         }).sort(function (a, b) {
             return b.acc - a.acc;
         }).slice(0, num_results);
+    // Hiển thị kết quả dự đoán
     document.getElementById("best-result").innerHTML = `Giá trị dự đoán: ${results[0].className} (Acc: ${results[0].acc.toFixed(2)}, Loss: ${(1 - results[0].acc).toFixed(2)})`;
+    // Ẩn loading spin
     hideSpin();
+    // Thực hiện di chuyển xuống cuối trang
     scrollDown();
+    // Hiển thị biểu đồ kết quả dự đoán
     //jsonChart(results);
-    document.getElementById('xem-them').style.display = 'block';
+    showXemThem();
 };
 
 
 async function loadDOM(num) {
+    /**
+     * Load giao diện Web
+     * @param num: Mã project
+     */
+    // Kiểm tra data về project có lưu trong LocalStorage không ? 
     if (localStorage.getItem(`${project.name}_project`) != null) {
         project = JSON.parse(localStorage.getItem(`${project.name}_project`));
     }
+    // Bắt đầu tải nếu chưa lưu 
     else {
         await $.getJSON('static/projects.json', function (data) {
             project = data[num];
             localStorage.setItem(`${project.name}_project`, project);
         });
     }
+    // Đặt thông tin cho trang
     $("meta[name='description']").attr("content", project.description);
     $("title").text(project.description);
     $("h4").text(project.description);
     $("a#ds-info").attr("href", project.dataset_url);
     $("a#ds-info").text(project.dataset_name);
+    // Tiếp tục tải thông tin các lớp của mô hình
     await loadClasses();
+    // Lấy thông tin kích thước ảnh đầu vào mà mô hình yêu cầu
     image_size = project.image_size.split('x');
     image_size = [parseInt(image_size[0]), parseInt(image_size[1])];
 };
 
 async function loadClasses() {
+    /**
+     * Load thông tin các lớp của mô hình
+     */
+    // Kiểm tra data về classes có lưu trong LocalStorage không ?
     if (localStorage.getItem(`${project.name}_classes`) != null) {
         classes = JSON.parse(localStorage.getItem(`${project.name}_classes`));
+    // Nếu chưa có, thực hiện tải lại từ nguồn
     } else {
         await $.get(project.classes, function (data) {
+            // Đọc thông tin các lớp từ file txt & tách từng lớp trên từng dòng
             let lines = data.split("\n");
+            // Thêm từng lớp vào mảng lưu trữ
             for (let i = 0, len = lines.length; i < len; i++) {
                 classes[i] = lines[i];
             }
+            // Ghi nhớ mảng lớp tại máy người dùng
             localStorage.setItem(`${project.name}_classes`, JSON.stringify(classes));
         }, 'text');
     }
+    // Chỉ nhận tối đa 5 kết quả đầu ra dự đoán nếu nhiều hơn 5 lớp
     num_classes = Object.keys(classes).length;
     if (num_classes <= 5) { num_results = num_classes }
 
